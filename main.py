df539c9321d05e7d4264792d020764648d43d3b1
lipeeeee <filipe.pereirafga@gmail.com>
2022-08-29 18:59:22 +0100

    gitignore and pygame window running

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import pygame
+import globals
 
+WIN = pygame.display.set_mode((globals.WIDTH, globals.HEIGHT))
+
+def game_loop():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
 
 if __name__ == '__main__':
-    pass
+    game_loop()
